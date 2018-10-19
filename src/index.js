66fb68698b3dc1b09825c2a88b07f3a82da7743f
Cassandra Matos <cassandra@matosmendez.com>
2018-10-19 08:22:48 -0700

    upload code base

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// import needed libraries
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// .render() & .createElement() syntax
+// ReactDOM.render(
+//   React.createElement(typeOfElement, { props }, children),
+//   elementLocation aka where do you want it to go
+// );
+
+// Create an h1 element that displays 'Hello World' and place it in the element that has the id 'hello'.
+
+// Create an h2 element that displays 'Bonjour' and has the CSS class of 'french' and place it in the element that has the id 'france'.
+
+//Create an ul element that displays a list with the following items listed:
+// Computer
+// Keyboard
+// Mouse
+// Strong Coffee
+// Sanity
+// and place it in the element that has the id of 'list'
+
